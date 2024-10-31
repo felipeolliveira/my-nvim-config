07724ddd8685ce9f02f1187745002a14babdf7e7
Felipe Oliveira <flpoliveira42@gmail.com>
2024-10-31 15:40:20 -0300

    feat: keymap to rename lsp definition

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,10 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- General
 vim.keymap.set("n", "<tab>", "<cmd>bnext<cr>", { desc = "Next Tab" })
 vim.keymap.set("n", "<S-tab>", "<cmd>bprevious<cr>", { desc = "Previous Tab" })
 vim.keymap.set({ "n", "i", "v", "o", "c" }, "<C-z>", "<cmd>u<cr>", { desc = "Undo with classic ctrl-z" })
+
+-- LSP
+vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, { desc = "Rename LSP definition" })
