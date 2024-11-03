01a9091fcae3163686ba8752349e8c8bef8b9c8a
Felipe Oliveira <flpoliveira42@gmail.com>
2024-11-03 15:42:30 -0300

    chore: update `keymaps.lua`
    Add `map` and `wk` local variables. Add terminal keymap with cwd. Add
    docker group with nerd font character

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,19 +2,25 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+local map = vim.keymap.set
+local wk = require("which-key")
+
 -- Navigation
-vim.keymap.set("n", "<tab>", "<cmd>bnext<cr>", { desc = "Next Tab" })
-vim.keymap.set("n", "<S-tab>", "<cmd>bprevious<cr>", { desc = "Previous Tab" })
-vim.keymap.set({ "n", "i", "v", "o", "c" }, "<C-z>", "<cmd>u<cr>", { desc = "Undo with classic ctrl-z" })
-vim.keymap.set("n", "<leader><space>", LazyVim.pick("files", { root = false }), { desc = "Find Files (cwd)" })
+map("n", "<tab>", "<cmd>bnext<cr>", { desc = "Next Tab" })
+map("n", "<S-tab>", "<cmd>bprevious<cr>", { desc = "Previous Tab" })
+map({ "n", "i", "v", "o", "c" }, "<C-z>", "<cmd>u<cr>", { desc = "Undo with classic ctrl-z" })
+map("n", "<leader><space>", LazyVim.pick("files", { root = false }), { desc = "Find Files (cwd)" })
 
 -- Files
-vim.keymap.set(
-  "n",
-  "<leader>fd",
-  "<cmd>call delete(expand('%')) | bdelete!<cr>",
-  { desc = "Delete current file and buffer" }
-)
+map("n", "<leader>fd", "<cmd>call delete(expand('%')) | bdelete!<cr>", { desc = "Delete current file and buffer" })
+
+-- Terminal
+map("n", "<C-/>", function()
+  LazyVim.terminal()
+end, { desc = "Terminal (cwd)" })
 
 -- Docker
-vim.keymap.set("n", "<leader>d", "<cmd>LazyDocker<cr>", { desc = "Toggle lazyDocker", noremap = true, silent = true })
+wk.add({
+  { "<leader>d", group = "docker", icon = { icon = "", color = "blue" } },
+})
+map("n", "<leader>dd", "<cmd>LazyDocker<cr>", { desc = "Toggle lazyDocker", noremap = true, silent = true })
