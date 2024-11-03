7e0f160e50c5f746eea3ff7141819384cf835e3f
Felipe Oliveira <flpoliveira42@gmail.com>
2024-11-03 15:02:23 -0300

    feat: add lazydocker and update keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,16 +2,19 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- General
+-- Navigation
 vim.keymap.set("n", "<tab>", "<cmd>bnext<cr>", { desc = "Next Tab" })
 vim.keymap.set("n", "<S-tab>", "<cmd>bprevious<cr>", { desc = "Previous Tab" })
 vim.keymap.set({ "n", "i", "v", "o", "c" }, "<C-z>", "<cmd>u<cr>", { desc = "Undo with classic ctrl-z" })
 vim.keymap.set("n", "<leader><space>", LazyVim.pick("files", { root = false }), { desc = "Find Files (cwd)" })
+
+-- Files
 vim.keymap.set(
   "n",
   "<leader>fd",
   "<cmd>call delete(expand('%')) | bdelete!<cr>",
   { desc = "Delete current file and buffer" }
 )
--- LSP
-vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, { desc = "Rename LSP definition" })
+
+-- Docker
+vim.keymap.set("n", "<leader>d", "<cmd>LazyDocker<cr>", { desc = "Toggle lazyDocker", noremap = true, silent = true })
