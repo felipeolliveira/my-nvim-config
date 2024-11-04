29ebc960b0296c694b1ba1dbc7b9cbc75abced07
Felipe Oliveira <flpoliveira42@gmail.com>
2024-11-04 16:03:03 -0300

    feat: add copilot

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -3,4 +3,6 @@ return {
   { import = "lazyvim.plugins.extras.lang.typescript" },
   { import = "lazyvim.plugins.extras.lang.prisma" },
   { import = "lazyvim.plugins.extras.lang.python" },
+  { import = "lazyvim.plugins.extras.coding.copilot" },
+  { import = "lazyvim.plugins.extras.coding.copilot-chat" },
 }
