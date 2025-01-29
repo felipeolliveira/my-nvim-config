d4184752108cc0c9142a107de8f373fc19b07129
Felipe Oliveira <flpoliveira42@gmail.com>
2025-01-29 17:09:25 -0300

    disable ai copilot

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -1,5 +1,5 @@
 return {
-  { import = "lazyvim.plugins.extras.ai.copilot" },
+  -- { import = "lazyvim.plugins.extras.ai.copilot" },
   { import = "lazyvim.plugins.extras.coding.luasnip" },
   { import = "lazyvim.plugins.extras.lsp.none-ls" },
   { import = "lazyvim.plugins.extras.lang.go" },
