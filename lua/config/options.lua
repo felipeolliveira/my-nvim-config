eff1ebed5f782ef1c71d44b8ac52cf1b770c34d8
Felipe Oliveira <flpoliveira42@gmail.com>
2025-08-20 16:30:38 -0300

    enable cursorline, add `numToStr/Comment.nvim`

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,4 +6,4 @@ vim.opt.guicursor =
   "a:block-blinkon0,i-c-ci:ver30-iCursor-blinkwait300-blinkon200-blinkoff150,r-cr-o:ver30-iCursor-blinkwait300-blinkon200-blinkoff150"
 vim.opt.relativenumber = false
 vim.opt.number = true
-vim.opt.cursorline = false
+-- vim.opt.cursorline = false
